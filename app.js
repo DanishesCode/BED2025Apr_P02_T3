b52882776daa3de3629d7c39d4cc743fd7cc2e14
DanishesCode <ezogetsthis@gmail.com>
2025-07-07 09:57:55 +0800

    refactor: cors error

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://127.0.0.1:5500/",
     credentials: true
 }));
 
